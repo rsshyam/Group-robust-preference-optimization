7245f5c8d06c371c2e9f3056d8a98b17726a0bf8
williambankes <williamjamesbankes@gmail.com>
2024-04-07 19:14:06 +0100

    using test experiments instead of experiments for e2e testing configs

--- test.py
+++ test.py
@@ -6,23 +6,23 @@ Created on Sun Apr  7 16:37:26 2024
 """
 
 import os
-import shutil
 import unittest
 from hydra import initialize, compose
 
 from train import main
 
-#Ensure the tests run in the order written
-unittest.TestLoader.sortTestMethodsUsing = None
+#Ensure the tests run in the order written:
+def cmp(a, b):
+    return (a > b) - (a < b) 
+unittest.TestLoader.sortTestMethodsUsing = lambda self, a, b: cmp(a, b) * -1
 
-class TestBasicTraining(unittest.TestCase):
+
+class End2EndTestCase(unittest.TestCase):
     
     @classmethod
     def setUpClass(self) -> None:
         
         self.dir_path = './test_outputs'
-        self.sft_save_dir = None
-        self.dpo_save_dir = None
 
         #Create the test outputs:        
         if not os.path.exists(self.dir_path):
@@ -30,58 +30,98 @@ class TestBasicTraining(unittest.TestCase):
         
     @classmethod
     def tearDownClass(self) -> None:
+                  
+        #Delete the resulting policy, optimizer and scheduler objects
+        file_path = os.path.join(self.dir_path, 'LATEST')
+        os.remove(os.path.join(file_path, 'policy.pt'))
+        os.remove(os.path.join(file_path, 'optimizer.pt'))
+        os.remove(os.path.join(file_path, 'scheduler.pt'))
+        os.remove(os.path.join(self.dir_path, 'config.yaml'))
         
-        for file in os.listdir(self.sft_save_dir):
-            print(file)
+        # Delete the LATEST folder
+        os.rmdir(file_path)
         
-    
-    def test_basic_sft(self) -> None:
-    
-        with initialize(version_base=None, config_path="./config"):
-            
-            # config is relative to a module
-            cfg = compose(config_name="config", overrides=["+experiment=test_sft"])
-            
-            # Create the save dir path:
-            self.sft_save_dir = cfg.local_run_dir                                
-            
-            #Run the e2e training script
-            main(cfg)
-
-    def test_basic_dpo(self) -> None:
+    def run_experiment_config(self, override_list):
         
         with initialize(version_base=None, config_path="./config"):
-            
-            # Build the sft training script archive path:
-            model_archive = os.path.join(self.sft_save_dir,
-                                         'LATEST', 'policy.pt')
-            
+                            
             # config is relative to a module
-            cfg = compose(config_name="config", overrides=["+experiment=test_sft",
-                                                           f"++model.archive={model_archive}"])
+            cfg = compose(config_name="config", overrides=override_list)
 
-            self.dpo_save_dir = cfg.local_run_dir             
+            #Set the script to save to the local dir path
+            cfg.local_run_dir = self.dir_path      
                         
             main(cfg)
-
-
-# class TestDataSelection(unittest.TestCase):
-            
-#     def test_basic_us_dpo(self) -> None:
         
-#         with initialize(version_base=None, config_path="./config"):
-            
-#             # config is relative to a module
-#             cfg = compose(config_name="config", overrides=["+experiment=test_sft"])
-#             main(cfg)
-            
-#     def test_basic_rho_loss_dpo(self): -> None:
+
+class TestBasicTraining(End2EndTestCase):
+    
+    """
+    Tests the Basic sft and dpo training scripts end to end. The experiment yaml
+    test_sft.yaml and test_dpo.yaml files contain suitable test experiment configs
+    """
         
-#         with initialize(version_base=None, config_path="./config"):
-            
-#             # config is relative to a module
-#             cfg = compose(config_name="config", overrides=["+experiment=test_sft"])
-#             main(cfg)
+    @classmethod
+    def setUpClass(self) -> None:
+        super().setUpClass()
+
+    @classmethod
+    def tearDownClass(self) -> None:
+        super().tearDownClass()
+        
+    def test_basic_sft(self) -> None:
+        """
+        We ensure that test_basic_sft is placed first in the class so as to create
+        the pretrained reference model setup in test_output dir that the other tests
+        can use...
+        """
+        self.run_experiment_config(override_list=["+test_experiment=test_sft"])
+    
+    def test_basic_dpo(self) -> None:
+        
+        model_archive = os.path.join(self.dir_path, 'LATEST', 'policy.pt')
+        
+        self.run_experiment_config(override_list=["+test_experiment=test_dpo",
+                                                 f"++model.archive={model_archive}"])
+        
+
+class TestDataSelection(End2EndTestCase):
+    
+    """
+    Test the e2e training scripts that run data selection code
+    """
+    
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+
+    @classmethod
+    def tearDownClass(self) -> None:
+        super().tearDownClass()
+        
+        
+    def test_basic_sft(self) -> None:
+        """
+        We ensure that test_basic_sft is placed first in the class so as to create
+        the pretrained reference model setup in test_output dir that the other tests
+        can use...
+        """
+        self.run_experiment_config(override_list=["+test_experiment=test_sft"])
+        
+    def test_basic_us_dpo(self) -> None:
+        
+        model_archive = os.path.join(self.dir_path, 'LATEST', 'policy.pt')
+        self.run_experiment_config(override_list=["+test_experiment=test_dpo_us",
+                                    f"++model.archive={model_archive}"])        
+
+    def test_basic_rho_loss_dpo(self) -> None:
+        
+        model_archive = os.path.join(self.dir_path, 'LATEST', 'policy.pt')
+        self.run_experiment_config(override_list=["+test_experiment=test_dpo_rho",
+                                    f"++model.archive={model_archive}",
+                                    "+model@data_selection.model=tiny-mistral",
+                                    f"++data_selection.ft_state_dict_path={model_archive}",
+                                    f"++data_selection.sft_state_dict_path={model_archive}"])
 
 if __name__ == '__main__':
     
